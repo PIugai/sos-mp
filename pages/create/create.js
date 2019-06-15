762465d6a3e3667bc2d94563862c74ba6fde3d36
Kelvin Lee <kelvin_lee@leaderradio.cn>
2019-06-15 19:03:20 +0800

    incomplete information on form now brings up error message toasts

--- pages/create/create.js
+++ pages/create/create.js
@@ -33,49 +33,82 @@ Page({
 
   // New Post Submission
   bindSubmit: function (e) {
-
-    wx.showToast({
-      title: 'Submitting...',
-      icon: 'loading',
-      duration: 1500
-    });
-
     const app = getApp();
 
-    var description = e.detail.value.description;
-    var capacity = parseInt(e.detail.value.capacity);
-    var contact_number = e.detail.value.contact_number;
-    if (e.detail.value.location === "") {
-      var location = "custom location";
+    if (e.detail.value.description === ""){
+      wx.showToast({
+        title: 'Need description',
+        icon: 'none',
+        duration: 1500
+      })
+    } else if (this.data.latitude === undefined){
+      wx.showToast({
+        title: 'Need location',
+        icon: 'none',
+        duration: 1500
+      })
+    } else if ((parseInt(e.detail.value.capacity).toString() !== e.detail.value.capacity) || (e.detail.value.capacity === undefined)){
+      wx.showToast({
+        title: 'Need number of people',
+        icon: 'none',
+        duration: 1500
+      })
+    } else if(e.detail.value.contact_number === ""){
+      wx.showToast({
+        title: 'Need phone number',
+        icon: 'none',
+        duration: 1500
+      })
     } else {
-      var location = e.detail.value.location;
-    };
-    var user_id = app.globalData.userId;
-    var latitude = this.data.latitude.toString();
-    var longitude = this.data.longitude.toString();
 
-    // Create new post
-    var event = {
-      // capacity should be an integer, userid shouldnt be undefined, 
-      "description": description,
-      "capacity": capacity,
-      "location": location,
-      "user_id": user_id,
-      "lat": latitude,
-      "long": longitude,
-    };
-    console.log(event);
-    wx.request({
-      url: `http://localhost:3000/posts`,
-      method: 'POST',
-      data: event,
-      success(res) {
-        wx.reLaunch({
-          url: '/pages/create/create',
-        })
-      }
-    })
+      wx.showToast({
+        title: 'Submitting...',
+        icon: 'loading',
+        duration: 1000
+      });
+
+      // storing all values from the form to create new post
+      var description = e.detail.value.description;
+      var capacity = parseInt(e.detail.value.capacity);
+      var contact_number = e.detail.value.contact_number;
+      if (e.detail.value.location === "") {
+        var location = "custom location";
+      } else {
+        var location = e.detail.value.location;
+      };
+      var user_id = app.globalData.userId;
+      var latitude = this.data.latitude.toString();
+      var longitude = this.data.longitude.toString();
+
+      // Create new post
+      var event = {
+        "description": description,
+        "capacity": capacity,
+        "location": location,
+        "user_id": user_id,
+        "lat": latitude,
+        "long": longitude,
+        "contact_number": contact_number,
+      };
+      console.log(event);
+      wx.request({
+        url: `http://localhost:3000/posts`,
+        method: 'POST',
+        data: event,
+        success(res) {
+          wx.reLaunch({
+            url: '/pages/create/create',
+          })
+        }
+      })
+    }
   },
+
+    
+
+
+    
+   
 
 
   goBack: function () {
